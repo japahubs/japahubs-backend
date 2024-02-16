e565656fc38da23b3642ef8955fb12a7e2c5493d
prosper20 <bobsonconnect@gmail.com>
2024-02-16 22:07:22 +0100

    chore: create user usecaase

---
+++ src/modules/users/domain/name.ts
@@ -0,0 +1,39 @@
+import { Result } from "../../../shared/core/Result";
+import { ValueObject } from "../../../shared/domain/ValueObject";
+import { Guard } from "../../../shared/core/Guard";
+
+interface NameProps {
+  value: string;
+}
+
+export class Name extends ValueObject<NameProps> {
+  public static maxLength: number = 30;
+  public static minLength: number = 2;
+
+  get value(): string {
+    return this.props.value;
+  }
+
+  private constructor(props: NameProps) {
+    super(props);
+  }
+
+  public static create(props: NameProps): Result<Name> {
+    const nameResult = Guard.againstNullOrUndefined(props.value, "name");
+    if (nameResult.isFailure) {
+      return Result.fail<Name>(nameResult.getErrorValue());
+    }
+
+    const minLengthResult = Guard.againstAtLeast(this.minLength, props.value);
+    if (minLengthResult.isFailure) {
+      return Result.fail<Name>(minLengthResult.getErrorValue());
+    }
+
+    const maxLengthResult = Guard.againstAtMost(this.maxLength, props.value);
+    if (maxLengthResult.isFailure) {
+      return Result.fail<Name>(minLengthResult.getErrorValue());
+    }
+
+    return Result.ok<Name>(new Name(props));
+  }
+}
---
+++ src/shared/domain/AggregateRoot.ts
@@ -0,0 +1,18 @@
+import { Entity } from "./Entity";
+import { IDomainEvent } from "./events/IDomainEvent";
+import { UniqueEntityID } from "./UniqueEntityID";
+
+export abstract class AggregateRoot<T> extends Entity<T> {
+  get id(): UniqueEntityID {
+    return this._id;
+  }
+
+  protected dispatchDomainEvent(domainEvent: IDomainEvent): void {
+    const eventData = domainEvent.toJSON();
+
+    // Pass the serialized event to the Kafka producer
+
+    // Log the domain event
+    console.info(`[Domain Event Created]:`, eventData);
+  }
+}
