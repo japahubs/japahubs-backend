61673111441de48939b57caacbd453c3cac7179e
prosper20 <bobsonconnect@gmail.com>
2024-02-18 15:11:12 +0100

    feature(user): implement login feature

--- tests/acceptance/users/registration.e2e.ts
+++ tests/acceptance/users/registration.e2e.ts
@@ -2,9 +2,12 @@ import { CreateUserDTO } from "src/modules/users/useCases/createUser/CreateUserD
 import { CompositionRoot } from "../../../src/shared/composition/compositionRoot";
 import { RESTfulAPIDriver } from "../../../src/shared/infra/http/restfulAPIDriver";
 import { UserBuilder } from "../../../src/shared/tests/users/builders/userBuilder";
+import { LoginDTO } from "src/modules/users/useCases/login/LoginDTO";
 
-describe("Registration End-to-End Test", () => {
+describe("Auth End-to-End Test", () => {
   let createUserInput: CreateUserDTO;
+  let CreateLoginUserInput: CreateUserDTO;
+  let loginUserInput: LoginDTO;
   let restfulAPIDriver: RESTfulAPIDriver;
   let server: any;
   let response: any;
@@ -16,7 +19,7 @@ describe("Registration End-to-End Test", () => {
     restfulAPIDriver = new RESTfulAPIDriver(server.getHttp());
   });
 
-  beforeEach(() => {
+  test("Successful registration", async () => {
     createUserInput = new UserBuilder()
       .withFirstName("Aliko")
       .withLastName("Dangote")
@@ -24,11 +27,31 @@ describe("Registration End-to-End Test", () => {
       .withPasswordConfirm("QWerty78")
       .withRandomEmail()
       .build();
-  });
 
-  test("Successful registration", async () => {
     response = await restfulAPIDriver.post("/users/new", createUserInput);
     expect(response.status).toBe(200);
+  });
+
+  test("Successful login", async () => {
+    CreateLoginUserInput = new UserBuilder()
+      .withFirstName("Daniel")
+      .withLastName("Regha")
+      .withPassword("QWerty78")
+      .withPasswordConfirm("QWerty78")
+      .withEmail("daniel.regha@mail.com")
+      .build();
+
+    await restfulAPIDriver.post("/users/new", CreateLoginUserInput);
+
+    loginUserInput = {
+      email: "daniel.Regha@mail.com",
+      password: "QWerty78",
+    };
+
+    response = await restfulAPIDriver.post("/users/login", loginUserInput);
+    expect(response.status).toBe(200);
+    expect(response.body.accessToken).toBeDefined();
+    expect(response.body.refreshToken).toBeDefined();
   });
 
   afterAll(async () => {
