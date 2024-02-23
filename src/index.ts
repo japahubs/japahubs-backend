1bbbd578779525bf9359b75e0add7af78727ee30
prosper20 <bobsonconnect@gmail.com>
2024-02-23 22:05:38 +0100

    refactor: renamed module from 'notification' to 'notifications'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { userRouter } from "./modules/users/infra/http/routes";
 import { config } from "./shared/config/appConfig.shared";
 import { WebServer } from "./shared/infra/http/webServer";
-import "./modules/notification/subscriptions";
+import "./modules/notifications/subscriptions";
 
 new WebServer({ port: config.api.port }, [userRouter]).start();
